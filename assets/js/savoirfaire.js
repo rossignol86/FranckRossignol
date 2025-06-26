fa30e92d8c9a5da35f11d9e696b36f90b76d266c
rossignol <162116711+rossignol86@users.noreply.github.com>
2025-06-26 15:02:34 +0200

    modification et creation de la page savoir faire

--- assets/js/savoirfaire.js
+++ assets/js/savoirfaire.js
@@ -1,8 +1,14 @@
-const images = document.querySelectorAll('.carousel-image');
-let current = 0;
+function initCarousel(className) {
+  const images = document.querySelectorAll(`.${className}`);
+  let current = 0;
 
-setInterval(() => {
-  images[current].classList.remove('active');
-  current = (current + 1) % images.length;
-  images[current].classList.add('active');
-}, 2000); // toutes les 2 secondes
+  setInterval(() => {
+    images[current].classList.remove('active');
+    current = (current + 1) % images.length;
+    images[current].classList.add('active');
+  }, 2000);
+}
+
+// Initialise les deux carrousels :
+initCarousel('carousel1-image');
+initCarousel('carousel2-image');
